29c6a917fc37e42548202ee531f90f1129291523
Christine Li <dev.nanayeli@gmail.com>
2025-05-16 10:56:25 +0630

    u

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Stopping existing container (if running)..."
-docker stop python-app-container || true
-docker rm python-app-container || true
+/usr/local/bin/docker stop python-app-container || true
+/usr/local/bin/docker rm python-app-container || true
 
 echo "Starting new container..."
-docker run -d --name python-app-container -p 5000:5000 python-app
+/usr/local/bin/docker run -d --name python-app-container -p 5000:5000 python-app
 
 echo "Deployment completed!"
